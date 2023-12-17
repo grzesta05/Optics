88bc57d5ca9e8739975b58e7e4990fd11e239558
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-17 19:58:45 +0100

    🔧 Update test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -7,4 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  test: {
+    environment: "happy-dom",
+  }
 });
