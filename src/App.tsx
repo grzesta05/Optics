8fcdf72c34edddb2a1de8725af6a7f6ecde6a144
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-21 16:48:21 +0100

    🔨 Add Start page to the index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "@styles/global.css";
-import { Home, About } from "@routes";
-import Start from "@routes/Start.tsx";
+import { Home, About, Start } from "@routes";
 
 function App() {
-	return (
-		<>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Home/>}/>
-					<Route path="/about" element={<About/>}/>
-					<Route path="/start" element={<Start/>}/>
-				</Routes>
-			</Router>
-		</>
-	);
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/start" element={<Start />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
