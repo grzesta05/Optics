db0ee1c88c5364852c3f24280857a052d14cc726
Bruhtek <bartosz.kas2005@gmail.com>
2024-02-04 14:43:12 +0100

    🐛Don't recreate object on every rerender

--- src/routes/Start.tsx
+++ src/routes/Start.tsx
@@ -3,11 +3,9 @@ import SimulationBoard from "@/Components/SimulationBoard";
 import SideWindow from "@/Components/SideWindow/SideWindow";
 import Laser from "@/model/SimulationObjects/Senders/Laser";
 import { useState } from "react";
-import Point from "@/classes/Point.ts";
 import SimulationObject from "@/model/SimulationObject.ts";
 
 function Start() {
-	const [offset, setOffset] = useState<Point>(new Point(0, 0));
 	const [objectsToRender, _] = useState<Array<SimulationObject>>([
 		new Laser(0, 0),
 		new Laser(200, 200),
@@ -16,8 +14,6 @@ function Start() {
 	return (
 		<div data-testid="StartScreen" className={styles.simulationContainer}>
 			<SimulationBoard
-				setOffset={setOffset}
-				offset={offset}
 				objectsToRender={objectsToRender}
 			/>
 			<SideWindow windows={[]}/>
