034618be80b93a46826aaeafbc0c44282c6897f0
Jakub Biela <jak.biela000@gmail.com>
2024-02-06 13:34:45 +0100

    add colors to the button UI component

--- src/Components/UI/Button.tsx
+++ src/Components/UI/Button.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import styles from "@styles/components/UI/Button.module.css";
 
 export type ButtonStyle = "rect" | "menutab" | "rounded";
+export type ButtonColor = "normal" | "error" | "danger" | "success";
 
 export type Props = {
 	className?: string,
@@ -9,14 +10,18 @@ export type Props = {
 	style?: React.StyleHTMLAttributes<any>,
 	disabled?: boolean,
 	styleType?: ButtonStyle
+	color?: ButtonColor,
 	children: ReactNode,
 };
 
 const Button = (props: Props) => {
 	const buttonStyleType = props.styleType ?? "rect";
+	const buttonColor = props.color ?? "normal";
 
 	return (
-		<button className={`${styles.button} ${styles[buttonStyleType]}`} { ...props }></button>
+		<button className={
+			`${styles.button} ${styles[buttonStyleType]} ${styles[buttonColor]}`
+		} { ...props } />
 	)
 };
 
